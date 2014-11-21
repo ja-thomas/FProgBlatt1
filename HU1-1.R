0abb2917ffc35ef79d9ad0b20c6532299914cecf
philippstats <philipp.stats@gmail.com>
2014-11-21 13:06:58 +0100

    Aufg1 style improved

--- HU1-1.R
+++ HU1-1.R
@@ -1,41 +1,40 @@
-# Hausübung 1
+# Fortgeschrittene Programmierung WS 2014/15
+# Hausübung 1 (due to 23.11.2014)
+# Janek Thomas, Philipp J. Rösch
 
-### 1 a
+### 1 a)
+
+# reads data from csv
 wtid <- read.csv("wtid-20141002.csv", 
-                 skip=1, header=TRUE)
-View(wtid)
-str(wtid)
+                 skip = 1, header = TRUE)
 
-### 1 b
+### 1 b)
 
+# keeps interested variables and rename them
 keeps <- c("Country", "Year", "P99.income.threshold", 
            "P99.5.income.threshold", "P99.9.income.threshold", 
            "P99.99.income.threshold")
 wtid <- wtid[keeps]
-str(wtid)
+colnames(wtid) <- c("country", "year", "p99", "p995", "p999", "p9999")
 
-colnames(wtid) <- c("country","year", "p99", "p995", "p999", "p9999")
+# gives 99.99% quantile from the United States for 1981 and 2001
+# single output
+wtid[wtid$country == "United States" & wtid$year == "1981", "p999"]
+wtid[wtid$country == "United States" & wtid$year == "2001", "p999"]
 
-wtid[wtid$country=="United States" & wtid$year=="1981", "p999"]
-wtid[wtid$country=="United States" & wtid$year=="2001", "p999"]
+#combined output with different methods
+wtid[wtid$country == "United States" & (wtid$year == "1981" | wtid$year == "2001"), "p999"]
+subset(wtid, country == "United States" & (wtid$year == "1981" | wtid$year == "2001"), 
+       select="p999", drop=TRUE)
 
-#NAs_per_row <- apply(wtid[,3:6], 1, function(x) sum(is.na(x)))
-#wtid$NAs_per_row <- NAs_per_row
+### 1 c)
 
-# Länder rauslöschen: 
+# remove countries with less than 20 complete observations
+complete_cases_indicator <- complete.cases(wtid[,3:6])
+country_indicator <- tapply(complete_cases_indicator, wtid$country, function(x) sum(x) >= 20)
+remaining_countries <- names(country_indicator[country_indicator==TRUE])
+wtid <-wtid[wtid$country %in% remaining_countries,]
 
-###
-wtid$cc <- complete.cases(wtid[,3:6])
-x <- tapply(wtid$cc, wtid$country, function(x) sum(x)>=20)
-str(x)
-
-wtid[x==TRUE]
-
-cnames <- names(x[x==TRUE])
-
-wtid$country %in% cnames
-
-w <-wtid[wtid$country %in% cnames,]
-
-w <- droplevels(w)
-levels(w$country)
+# drop levels from removed countries
+wtid <- droplevels(wtid)
+levels(wtid$country)
